0d829e48f1af2fbd3a56f20c6cd2f3d9c60618ca
khanhn-1040 <nguyen.khanh@sun-asterisk.com>
2025-05-09 17:19:33 +0700

    feat: ui widget input

--- frontend/src/pages/Login/ui/Login.tsx
+++ frontend/src/pages/Login/ui/Login.tsx
@@ -1,48 +1,18 @@
 import { Link } from 'react-router-dom';
+import {Input} from "@widgets/form/ui/Input.tsx";
+import {useLoginForm} from "../model/Login.ts";
 
 export const Login = () => {
+  const {formData, handleChange} = useLoginForm();
+
   return (
     <div className="flex min-h-screen items-center justify-center bg-gray-100 px-4">
       <div className="w-full max-w-lg space-y-6">
         <form className="space-y-6 rounded-xl bg-white p-6 shadow-md">
           {/* Email */}
-          <div>
-            <label
-              htmlFor="email"
-              className="block text-sm font-medium text-gray-700"
-            >
-              Email
-            </label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              className="mt-1 w-full rounded-md border border-gray-300 px-4 py-2 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-              placeholder="you@example.com"
-            />
-            <p className="mt-1 text-sm text-red-600">
-              Please enter a valid email
-            </p>
-          </div>
-
+          <Input label="Email" type="email" name="email" value={formData.email} placeholder="you@example.com" onChange={handleChange} />
           {/* Password */}
-          <div>
-            <label
-              htmlFor="password"
-              className="block text-sm font-medium text-gray-700"
-            >
-              Password
-            </label>
-            <input
-              type="password"
-              id="password"
-              name="password"
-              className="mt-1 w-full rounded-md border border-gray-300 px-4 py-2 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-              placeholder="••••••••"
-            />
-            <p className="mt-1 text-sm text-red-600">Password is required</p>
-          </div>
-
+          <Input label="Password" type="password" name="password" value={formData.password} placeholder="••••••••" onChange={handleChange} />
           {/* Submit Button */}
           <div>
             <button
---
+++ frontend/src/widgets/form/ui/Input.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+
+interface InputProps {
+  label?: string;
+  name: string;
+  type?: string;
+  placeholder?: string;
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  error?: string;
+}
+
+export const Input: React.FC<InputProps> = ({
+  label,
+  name,
+  type = 'text',
+  placeholder = '',
+  value,
+  onChange,
+  error,
+}) => {
+  return (
+    <>
+      <div>
+        {label && (
+          <label
+            htmlFor={name}
+            className="block text-sm font-medium text-gray-700"
+          >
+              {label}
+          </label>
+        )}
+        <input
+          type={type}
+          id={name}
+          name={name}
+          value={value}
+          onChange={onChange}
+          className="mt-1 w-full rounded-md border border-gray-300 px-4 py-2 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+          placeholder={placeholder}
+        />
+        <p className="mt-1 text-sm text-red-600">{error}</p>
+      </div>
+    </>
+  );
+};
