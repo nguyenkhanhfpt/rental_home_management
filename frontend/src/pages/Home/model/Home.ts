f004d2b34c111ed8d29233400f830bc5516b01e5
khanhn-1040 <nguyen.khanh@sun-asterisk.com>
2025-05-31 21:31:16 +0700

    feat: home UI list

--- frontend/src/pages/Home/model/Home.ts
+++ frontend/src/pages/Home/model/Home.ts
@@ -1,17 +1,37 @@
 import { useState } from 'react';
 import { Home, useHome } from '@/shared/hooks/useHome';
 
-const initHomeState: Home[] = [];
+const initHomesState: Home[] = [];
+const initHomeState: Home | null = null;
 
 export const useHomeFeature = () => {
-  const [homes, setHomes] = useState<Home[]>(initHomeState);
-  const { fetchHomes } = useHome();
+  const [homes, setHomes] = useState<Home[]>(initHomesState);
+  const [home, setHome] = useState<Home | null>(initHomeState);
+  const [searchHome, setSearchHome] = useState('');
+  const { fetchHomes, fetchHome } = useHome();
 
   const getHomes = async () => {
     try {
       // Simulate fetching homes from an API
       const response = await fetchHomes();
-      setHomes(response); // Assuming the response data is an array of Home objects
+      setHomes(response as Home[]); // Assuming the response data is an array of Home objects
+    } catch (err: any) {
+      // setError(err.message || "Failed to fetch homes");
+    } finally {
+      // setLoading(false);
+    }
+  };
+
+  const changeSearchHome = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+    setSearchHome(value);
+  };
+
+  const getHomeDetail = async (homeId: number) => {
+    try {
+      // Simulate fetching homes from an API
+      const response = await fetchHome(homeId);
+      setHome(response as Home); // Assuming the response data is an array of Home objects
     } catch (err: any) {
       // setError(err.message || "Failed to fetch homes");
     } finally {
@@ -24,5 +44,9 @@ export const useHomeFeature = () => {
     homes,
     loading: false,
     error: null,
+    searchHome,
+    changeSearchHome,
+    getHomeDetail,
+    home
   };
 };
